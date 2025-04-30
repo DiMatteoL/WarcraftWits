4c779c063bb0dc03be5f25ae4ba3a93cc2677747
DiMatteoL <luca7dimatteo@gmail.com>
2025-04-30 21:18:38 +0200

    not auto ad

--- components/Adsense-ad.tsx
+++ components/Adsense-ad.tsx
@@ -13,11 +13,11 @@ export const AdsenseAd = () => {
             <div className="absolute inset-0 pt-2 border-border border-t self-end">
                 <ins
                     className="adsbygoogle"
-                    style={{ display: "block" }}
+                    style={{ display: "block", height: "250px", width: "100%" }}
                     data-ad-client="ca-pub-5164534018223080"
                     data-ad-slot="8089054608"
                     data-ad-format="auto"
-                    data-full-width-responsive="true"
+                    data-full-width-responsive="false"
                 />
             </div>
         </div>
